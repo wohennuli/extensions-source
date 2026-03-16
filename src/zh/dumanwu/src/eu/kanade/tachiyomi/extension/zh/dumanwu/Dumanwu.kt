ee8eb90c9cfa3ba63c818dafb72dc8663e4969e1
xiaolan <103926807+wohennuli@users.noreply.github.com>
2026-03-16 16:11:55 +0800

    Update Dumanwu.kt

--- src/zh/dumanwu/src/eu/kanade/tachiyomi/extension/zh/dumanwu/Dumanwu.kt
+++ src/zh/dumanwu/src/eu/kanade/tachiyomi/extension/zh/dumanwu/Dumanwu.kt
@@ -5,7 +5,7 @@ import eu.kanade.tachiyomi.multisrc.mmlook.MMLook
 class Dumanwu :
     MMLook(
         "读漫屋",
-        "https://m.dumanwu1.com",
-        "https://www.dumanwu1.com",
+        "http://m.dumanwu1.com",
+        "http://www.dumanwu1.com",
         useLegacyMangaUrl = false,
     )
